bc446f62fc01feaeb8c7e41b9d58d15312a37ff5
Ian <iankimeli@gmail.com>
2021-02-27 17:11:02 +0300

    about us section

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(()=>{
+   // let myForm = $("#myForm")
+function validate(){
+    let name = $('#name').val();
+    let email = $('#email').val();
+    let message= $('#message').val();
+
+    if {name =="" || email =='' || message ==''}
+        alert{'Error! All fields must be filled'}
+        return false
+}
+})
