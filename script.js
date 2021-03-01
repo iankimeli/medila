52d66ce9ab1bd3d6a4a12a0bfeb92a2a35cfc1f9
Ian <iankimeli@gmail.com>
2021-03-01 22:25:32 +0300

    added dev and prop functions

--- script.js
+++ script.js
@@ -23,24 +23,45 @@ $(function () {
     $(prop).hide();
 
     $(".des").on("click", function (){
-        $(".des").slideup("slow");
+        $(".des").slideUp("slow");
         $(des).show(500);
         $(destext).animate({
             fontweight:"bolder", fontsize:"30px"
         })
     });
     $(".dev").on("click", function (){
-        $(".dev").slideup("slow");
+        $(".dev").slideUp("slow");
         $(devp).show(500);
         $(devtext).animate({
             fontweight:"bolder", fontsize:"30px"
         })
     });
     $(".pro").on("click", function (){
-        $(".pro").slideup("slow");
+        $(".pro").slideUp("slow");
         $(prop).show(500);
         $(protext).animate({
             fontweight:"bolder", fontsize:"30px"
+        })
+    });
+    $(desp, desptext).on("click", function () {
+        $(".des").slideDown("slow");
+        $(desp).hide(500);
+        $(destext).animate({
+            fontweight: "normal", fontsize: "16px"
+        })
+    });
+    $(devp, devtext).on("click", function () {
+        $(".dev").slideDown("slow");
+        $(devp).hide(500);
+        $(devtext).animate({
+            fontweight: "normal", fontsize: "16px"
+        })
+    });
+    $(prop, protext).on("click", function () {
+        $(".pro").slideDown("slow");
+        $(prop).hide(500);
+        $(protext).animate({
+            fontweight: "normal", fontsize: "16px"
         })
     });
 })
